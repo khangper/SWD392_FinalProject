a63e8c72f87e8b84c40bf66b4a8ee6b160952e31
Hao Nguyen <155703060+haonnse172116@users.noreply.github.com>
2024-06-22 15:55:13 +0700

    update layout

--- src/App.js
+++ src/App.js
@@ -5,25 +5,23 @@ import { Navigate, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home/Home";
 import Login from "./pages/Landing/Login/Login";
 import ForgotPassword from "./pages/Landing/ForgotPassword/ForgotPassword";
+import Layout from "./components/Layout/Layout";
 
 function App() {
   const [sidebar, setSidebar] = useState(true);
   return (
     <div>
-      <Routes>
-        <Route path="/" element={<Navigate to="/login" />} />
-        <Route path="/login" element={<Login />} />
-        <Route
-          path="/home"
-          element={
-            <div>
-              <Navbar setSidebar={setSidebar} />
-              <Home sidebar={sidebar} />
-            </div>
-          }
-        />
-        <Route path="/forgot-password" element={<ForgotPassword />} />
-      </Routes>
+       <Routes>
+       <Route path="/" element={<Navigate to="/login" />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/forgot-password" element={<ForgotPassword />} />
+      <Route
+        path="/"
+        element={<Layout sidebar={sidebar} setSidebar={setSidebar} />}
+      >
+        <Route path="home" element={<Home />} />
+      </Route>
+    </Routes>
     </div>
   );
 }
