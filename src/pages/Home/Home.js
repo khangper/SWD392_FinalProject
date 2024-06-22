e13c4afc5aac8d2d0cd99b12e2e7f935f15a90da
Hao Nguyen <155703060+haonnse172116@users.noreply.github.com>
2024-06-22 19:59:55 +0700

    update

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,12 +1,13 @@
 import React from 'react'
 import Sidebar from '../../components/Sidebar/Sidebar'
 import './Home.css'
-import Footer from '../../components/Footer/Footer'
 const Home = () => {
   return (
+
     <div>
         Livestream
-    </div>
+      </div>
+   
 
   )
 }
