e0c2ba7311ec0bf60c65ee41b066ff525605f646
Hao Nguyen <155703060+haonnse172116@users.noreply.github.com>
2024-07-07 04:30:42 +0700

    add analytics

---
+++ src/pages/Analytics/chartComponent/BarChart.js
@@ -0,0 +1,63 @@
+import React from 'react';
+import { Bar } from 'react-chartjs-2';
+import { Chart as ChartJS, CategoryScale, LinearScale, BarElement, Tooltip, Legend } from 'chart.js';
+
+ChartJS.register(CategoryScale, LinearScale, BarElement, Tooltip, Legend);
+
+const BarChart = () => {
+  const data = {
+    labels: ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"],
+    datasets: [
+      {
+        label: "Subscribers",
+        data: [6, 3, 4, 3, 6, 9, 4, 8, 9, 5, 8, 3, 4],
+        backgroundColor: "#ed2a26"
+      }
+    ]
+  };
+
+  const options = {
+    responsive: true,
+    maintainAspectRatio: false,
+    plugins: {
+      legend: {
+        display: false,
+      },
+      tooltip: {
+        callbacks: {
+          label: function (context) {
+            return context.raw;
+          },
+        },
+      },
+    },
+    scales: {
+      x: {
+        grid: {
+          display: false,
+          drawBorder: false,
+        },
+        ticks: {
+          display: false,
+        },
+      },
+      y: {
+        grid: {
+          display: false,
+          drawBorder: false,
+        },
+        ticks: {
+          display: false,
+        },
+      },
+    },
+  };
+
+  return (
+    <div style={{ height: '200px', width: '300px' }}>
+      <Bar data={data} options={options} />
+    </div>
+  );
+};
+
+export default BarChart;
