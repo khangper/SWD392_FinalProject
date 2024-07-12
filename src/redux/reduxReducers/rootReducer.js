ef24b9a03a8ffacdcc6e8d8f1eba1ec07232343e
Nguyễn Trung Tín <nttin20@clc.fitus.edu.vn>
2024-07-12 11:02:40 +0700

    commit redux searchresult

--- src/redux/reduxReducers/rootReducer.js
+++ src/redux/reduxReducers/rootReducer.js
@@ -1,14 +1,16 @@
 import { combineReducers } from "redux";
 import streamsReducer from "./reducers/AllLiveStreamReducer";
 import instructorReducer from "./reducers/instructorReducer";
-  import HomeLivestreamReducer from './reducers/homeReducers/HomeLivestreamReducer';
+import HomeLivestreamReducer from './reducers/homeReducers/HomeLivestreamReducer';
 import HomeFeaturedCourseReducer from './reducers/homeReducers/HomeFeatureCourseReducer';
+import searchresultReducer from "./reducers/searchresultReducer";
 
 const rootReducer = combineReducers({
-    home_livestream: HomeLivestreamReducer,
-    home_featuredcourse: HomeFeaturedCourseReducer,
+  home_livestream: HomeLivestreamReducer,
+  home_featuredcourse: HomeFeaturedCourseReducer,
   streams: streamsReducer,
   allinstructor: instructorReducer,
+  searchresult: searchresultReducer,
 });
 
 export default rootReducer;
--- src/redux/reduxSagas/rootSaga.js
+++ src/redux/reduxSagas/rootSaga.js
@@ -2,13 +2,15 @@ import { all } from "redux-saga/effects";
 
 import streamsSaga from "./sagas/AllLiveStreamSaga";
 import instructorsSaga from "./sagas/instructorsSaga";
-  import { watchFetchHomeLiveStreams } from "./sagas/homeSagas/HomeLivestreamSaga";
+import { watchFetchHomeLiveStreams } from "./sagas/homeSagas/HomeLivestreamSaga";
 import { watchFetchHomeFeaturedCourses } from "./sagas/homeSagas/HomeFeaturedCourseSaga";
+import searchresultSaga from "./sagas/searchresultSaga";
 
 export default function* rootSaga() {
-  yield all([streamsSaga(), 
-             instructorsSaga(),
-             watchFetchHomeLiveStreams(),
-              watchFetchHomeFeaturedCourses(),
-            ]);
+  yield all([streamsSaga(),
+  instructorsSaga(),
+  watchFetchHomeLiveStreams(),
+  watchFetchHomeFeaturedCourses(),
+  searchresultSaga(),
+  ]);
 }
