0bdb9755e54a4c01ff145c068a1376eb96bd4137
Hao Nguyen <155703060+haonnse172116@users.noreply.github.com>
2024-07-23 01:43:58 +0700

    implement one-sided email: recipent: nhathao189203@gmail.com

--- src/pages/SendFeedback/SendFeedback.js
+++ src/pages/SendFeedback/SendFeedback.js
@@ -1,23 +1,45 @@
-import React from 'react'
+import React, { useState } from 'react'
 import ".//SendFeedback.css"
 import send_feedback from '..//../assets/send-feedback.png';
+import emailjs from '@emailjs/browser';
+
 export default function SendFeedback() {
+    const [email, setEmail] = useState('');
+  const [description, setDescription] = useState('');
+  const [loading, setLoading] = useState(false);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setLoading(true);
+
+    emailjs.sendForm('service_mail_haonn', 'template_haonn', e.target, 'ZvSJNf8vhDCcwsC_Y')
+      .then((result) => {
+          setLoading(false);
+      }, (error) => {
+          setLoading(false);
+      });
+  };
+
   return (
     <div className="feedback-container">
     <h2 className="Feedback_title">
         <img className="sidebar-icon" src={send_feedback} alt="Send Feedback Icon" />  Send Feedback
     </h2>
     <div className="feedback-grid">
-        <div className="feedback-form">
+        <form className="feedback-form" onSubmit={handleSubmit}>
             <div className="feedback-email-from">
                 <div className="email-address">
-                    <input className="Email-Sendfeeback" type="email" name="emailaddress" id="id_email" required="" maxLength="64" placeholder="Email address"/>                                                            
+                    <input className="Email-Sendfeeback" type="email" name="emailaddress" id="id_email" required maxLength="64" placeholder="Email address"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}/>                                                            
                 </div>
             </div>
             <div className="Describe-feedback">                                                                
                 <div className="ui form swdh30">
                     <div className="field">
-                        <textarea rows="6" name="description" id="id_about" placeholder="Describe your issue or share your ideas"></textarea>
+                        <textarea rows="6" name="message" id="id_about" placeholder="Describe your issue or share your ideas"
+                         value={description}
+                         onChange={(e) => setDescription(e.target.value)}></textarea>
                     </div>
                 </div>
             </div>
@@ -32,8 +54,8 @@ export default function SendFeedback() {
                     </div>
                 </div>                                                            
             </div>
-            <button className="SendFeedback_btn" type="submit">Send Feedback</button>
-        </div>
+            <button className="SendFeedback_btn" type="submit" disabled={loading}>{loading ? 'Loading...' : 'Send Feedback'}</button>
+        </form>
     </div>                      
 </div>
 
