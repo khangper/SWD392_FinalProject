a232e727785ba702aeba800e7d7fbcbe6512856e
Hao Nguyen <155703060+haonnse172116@users.noreply.github.com>
2024-07-26 08:29:00 +0700

    adjust saga and home text

--- src/redux/reduxSagas/sagas/CreateCourseSaga.js
+++ src/redux/reduxSagas/sagas/CreateCourseSaga.js
@@ -1,9 +1,11 @@
 import { call, put, takeLatest } from "redux-saga/effects";
 import { API_KEY, CREATE_COURSE_REQUEST } from "../../../constant/data";
 import { createCourseFailure, createCourseSuccess } from "../../reduxActions/CreateCourseAction";
+import { startLoading, stopLoading } from "../../reduxActions/LoadingAction";
 
 function* createCourseSaga(action) {
     try {
+      yield put(startLoading());
       const response = yield call(fetch,  `${API_KEY}home_featuredcourse`, {
         method: 'POST',
         headers: {
@@ -14,11 +16,14 @@ function* createCourseSaga(action) {
       const data = yield response.json();
       if (response.ok) {
         yield put(createCourseSuccess(data));
+        yield put(stopLoading());
       } else {
         yield put(createCourseFailure(data.error));
+        yield put(stopLoading());
       }
     } catch (error) {
       yield put(createCourseFailure(error.message));
+      yield put(stopLoading());
     }
   }
 
