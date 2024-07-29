5ea66db25931195f435973b9546935dc1b68c471
khangper <89836419+khangper@users.noreply.github.com>
2024-07-29 17:08:46 +0700

    UnitTestCreateNewCourse

--- src/pages/CreateNewCourse/CreateNewCourse.test.js
+++ src/pages/CreateNewCourse/CreateNewCourse.test.js
@@ -3,7 +3,7 @@ import { render, screen } from "@testing-library/react";
 import CreateNewCourse from "../CreateNewCourse/CreateNewCourse";
 import "@testing-library/jest-dom/extend-expect";
 import { renderWithProviders } from "../../constant/test-util";
-
+import userEvent from "@testing-library/user-event";
 describe("CreateNewCourse", () => {
   beforeAll(() => {
     jest.spyOn(console, "error").mockImplementation(() => {});
@@ -18,5 +18,27 @@ describe("CreateNewCourse", () => {
     const { container } = renderWithProviders(<CreateNewCourse />);
     expect(container).toBeDefined();
     expect(container).toMatchSnapshot();
+  });
+
+  // Course title here
+
+  it("handles whitespace and special characters in the title input", async () => {
+    renderWithProviders(<CreateNewCourse />);
+
+    const titleInput = screen.getByPlaceholderText("Course title here");
+
+    // Simulate entering whitespace
+    userEvent.type(titleInput, "   ");
+    expect(titleInput.value).toBe("   ");
+
+    // Simulate entering special characters
+    userEvent.clear(titleInput);
+    userEvent.type(titleInput, "!@#$%^&*()_+");
+    expect(titleInput.value).toBe("!@#$%^&*()_+");
+
+    // Simulate entering a mix of whitespace and special characters
+    userEvent.clear(titleInput);
+    userEvent.type(titleInput, "   !@#$%^&*()_+   ");
+    expect(titleInput.value).toBe("   !@#$%^&*()_+   ");
   });
 });
